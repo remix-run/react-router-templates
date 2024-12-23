028c16e4f57737d13b58c4c777531efb6543c6d8
Lavish Thakkar <lavisht22@gmail.com>
2024-12-23 10:56:27 +0530

    Add template for cloudflare pages

---
+++ cloudflare-pages/vite.config.ts
@@ -0,0 +1,31 @@
+import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  ssr: {
+    target: "webworker",
+    noExternal: true,
+    resolve: {
+      conditions: ["workerd", "browser"],
+    },
+    optimizeDeps: {
+      include: [
+        "react",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "react-dom",
+        "react-dom/server",
+        "react-router",
+      ],
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+});
