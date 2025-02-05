5cb94e36c6469d9ac4741994d83f592287c6a5f1
Piero Giusti <pierophp@gmail.com>
2025-02-05 18:39:07 -0300

    load context

--- cloudflare/vite.config.ts
+++ cloudflare/vite.config.ts
@@ -4,13 +4,30 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./workers/app.ts",
-        }
-      : undefined,
+// By default react-router's dev server uses Node.js, so we want to remove their server
+// configuration to use the dev server provided by Vite + Workerd.
+const reactRouterPlugins = reactRouter();
+const reactRouterPlugin = reactRouterPlugins.find(
+  (plugin) => plugin.name === "react-router"
+)!;
+reactRouterPlugin.configureServer = undefined;
+
+export default defineConfig({
+  // css: {
+  //   postcss: {
+  //     plugins: [tailwindcss],
+  //   },
+  // },
+  plugins: [cloudflare(), tailwindcss(), reactRouterPlugins, tsconfigPaths()],
+  ssr: {
+    resolve: {
+      conditions: ["workerd", "worker", "browser"],
+    },
   },
-  plugins: [cloudflare(), tailwindcss(), reactRouter(), tsconfigPaths()],
-}));
+  resolve: {
+    mainFields: ["browser", "module", "main"],
+  },
+  build: {
+    minify: true,
+  },
+});
