3f134df99ae53c44f2596cba684efb2a57c8c125
Alexandr Sashin <alexandrsashin@gmail.com>
2025-03-16 16:32:22 +0300

    Added favicon link to root.tsx

--- default/app/root.tsx
+++ default/app/root.tsx
@@ -21,6 +21,12 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    href: "/public/favicon.ico",
+    sizes: "48x48",
+    type: "image/vnd.microsoft.icon"
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
