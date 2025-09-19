ada619ec74d88bc4a0ba655e4a342042dae4f3c5
Jacob Ebey <jacob.ebey@live.com>
2025-09-19 13:42:36 -0700

    simplify rsc framework template and use react-router-serve

--- unstable_rsc-framework-mode/vite.config.ts
+++ unstable_rsc-framework-mode/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
 import { unstable_reactRouterRSC as reactRouterRSC } from "@react-router/dev/vite";
-import rsc from "@vitejs/plugin-rsc";
 import tailwindcss from "@tailwindcss/vite";
+import rsc from "@vitejs/plugin-rsc";
+import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    // Experimental RSC plugins
     reactRouterRSC(),
     rsc(),
+    // Chrome DevTools support
+    devtoolsJson(),
   ],
 });
